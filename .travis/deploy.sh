550f5baba6b6aee67513922db60144f4cb296495
Pudding <s.kokuryo@gmail.com>
2018-10-19 20:25:46 +0900

    add git pull before deploy

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # normal deploy
-COMMAND="cd /opt/KB_1809_2 && make pull && make qa-manage ARGS=migrate && docker-compose up -d --build"
+COMMAND="cd /opt/KB_1809_2 && sudo git pull && make pull && make qa-manage ARGS=migrate && docker-compose up -d --build"
 
 # exec
 ssh -o StrictHostKeyChecking=no -i /tmp/aquatan_studio deploy@aqua.aquatan.studio ${COMMAND}
