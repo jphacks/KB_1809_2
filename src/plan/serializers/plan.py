071bb64a405a159b52306c731786998aae761442
Pudding <s.kokuryo@gmail.com>
2018-10-21 01:06:03 +0900

    refs #31 implement get list of Plan

--- src/plan/serializers/plan.py
+++ src/plan/serializers/plan.py
@@ -6,6 +6,27 @@ from .fav import FavSerializer
 from .comment import CommentSerializer
 from .report import ReportSerializer
 from .location import LocationSerializer
+from accounts.serializers import SimpleUserSerializer
+
+
+class AbstractPlanSerializer(serializers.ModelSerializer):
+    """
+    List表示用のPlanSerializer
+    """
+    spots = SpotSerializer(many=True, allow_null=True)
+    user = SimpleUserSerializer()
+    location = LocationSerializer()
+
+    class Meta:
+        model = Plan
+        fields = ('pk', 'name', 'price', 'duration', 'note', 'location', 'spots',
+                  'created_at', 'user', 'favorite_count', 'comment_count')
+
+    def to_representation(self, instance):
+        res = super(AbstractPlanSerializer, self).to_representation(instance)
+        all_spots = res['spots']
+        res['spots'] = [all_spots[0], all_spots[-1]]
+        return res
 
 
 class PlanSerializer(serializers.ModelSerializer):
--- src/plan/views.py
+++ src/plan/views.py
@@ -1,6 +1,9 @@
 from rest_framework import viewsets
+from rest_framework import generics
+from rest_framework.decorators import action
 from rest_framework.parsers import JSONParser
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from django_filters import rest_framework as filters
 
 from . import models, serializers
@@ -57,3 +60,9 @@ class PlanViewSets(viewsets.ModelViewSet):
     parser_classes = (JSONParser,)
     serializer_class = serializers.PlanSerializer
     permission_classes = (IsAuthenticated,)
+
+    def list(self, request, *args, **kwargs):
+        """一覧表示の場合は情報を削減したSerializerを使用する"""
+        serializer = serializers.AbstractPlanSerializer(self.queryset, many=True)
+        return Response(serializer.data)
+
