2c4c5113b3b285d198742bdc02590ca188cd23d8
Pudding <s.kokuryo@gmail.com>
2018-10-20 23:48:54 +0900

    refs #31 implement PlanSerializer

---
+++ src/plan/serializers/plan.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+
+from plan.models import Plan
+from .spot import SpotSerializer
+from .fav import FavSerializer
+from .comment import CommentSerializer
+from .report import ReportSerializer
+from .location import LocationSerializer
+
+
+class PlanSerializer(serializers.ModelSerializer):
+    """
+    単一のPlanの処理
+    """
+
+    spots = SpotSerializer(many=True, allow_null=True)
+    favs = FavSerializer(many=True, allow_null=True)
+    comments = CommentSerializer(many=True, allow_null=True)
+    reports = ReportSerializer(many=True, allow_null=True)
+    location = LocationSerializer()
+
+    class Meta:
+        model = Plan
+        fields = ('pk', 'name', 'price', 'duration', 'note', 'location',
+                  'reports', 'favs', 'created_at', 'comments', 'spots')
