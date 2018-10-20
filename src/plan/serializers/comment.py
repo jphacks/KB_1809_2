64dcc9acf2eb353ab03dae78e9bb375f7b65a961
taxio <taxio.2029@gmail.com>
2018-10-20 17:34:29 +0900

    add simple comment serializer

---
+++ src/plan/serializers/comment.py
@@ -0,0 +1,33 @@
+from rest_framework import serializers
+
+from plan.models import Comment, Plan
+from accounts.models import User
+
+class CommentSerializer(serializers.ModelSerializer):
+    """
+    単一のCommentを処理するSerializer
+    """
+
+    def to_internal_value(self, data):
+        user_id = data.get('user_id')
+        plan_id = data.get('plan_id')
+        text = data.get('text')
+        if not user_id:
+            raise serializers.ValidationError({'user_id': 'This field is required.'})
+        if not plan_id:
+            raise serializers.ValidationError({'plan_id': 'This field is required.'})
+        if not text or len(text) == 0:
+            raise serializers.ValidationError({'text': 'This field is required.'})
+
+        return {
+            'user_id': user_id,
+            'plan_id': plan_id,
+            'text': text,
+        }
+
+    def create(self, validated_data):
+        user = User.objects.get(pk=validated_data.get('user_id'))
+        plan = Plan.objects.get(pk=validated_data.get('plan_id'))
+        text = validated_data.get('text')
+
+        return Comment.objects.create(user=user, plan=plan, text=text)
