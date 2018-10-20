202ea7fd9f9542f7f3aa26e1c885e0738914b3e0
Pudding <s.kokuryo@gmail.com>
2018-10-20 12:19:10 +0900

    fix deploy script

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # normal deploy
-COMMAND="cd /opt/KB_1809_2 && sudo git pull && make pull && make qa-manage ARGS=migrate && docker-compose -f docker-compose.prod.yml up -d --build"
+COMMAND="cd /opt/KB_1809_2 && sudo git pull && make pull && docker-compose -f docker-compose.prod.yml up -d --build && make qa-manage ARGS=migrate"
 
 # exec
 ssh -o StrictHostKeyChecking=no -i /tmp/aquatan_studio deploy@aqua.aquatan.studio ${COMMAND}
