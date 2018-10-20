d401bca51794f5c0944b309d5b6a21b6ccaffb03
Pudding <s.kokuryo@gmail.com>
2018-10-21 04:10:07 +0900

    close #31 implement create plan endpoint

--- src/plan/serializers/plan.py
+++ src/plan/serializers/plan.py
@@ -1,11 +1,12 @@
 from rest_framework import serializers
 
-from plan.models import Plan
+from plan.models import Plan, Spot, Location
 from .spot import SpotSerializer
 from .fav import FavSerializer
 from .comment import CommentSerializer
 from .report import ReportSerializer
 from .location import LocationSerializer
+from plan.geo import convert_geo_to_location
 from accounts.serializers import SimpleUserSerializer
 
 
@@ -47,3 +48,33 @@ class PlanSerializer(serializers.ModelSerializer):
         fields = ('pk', 'name', 'price', 'duration', 'note', 'location',
                   'reports', 'favs', 'created_at', 'comments', 'spots', 'user',
                   'favorite_count', 'comment_count')
+
+    def to_internal_value(self, data):
+        res = {}
+        fields = ('name', 'price', 'duration', 'note', 'spots')
+        for key in fields:
+            if key not in data:
+                raise serializers.ValidationError({key: "This field is required."})
+            res[key] = data[key]
+        return res
+
+    def create(self, validated_data):
+        user = self.context['request'].user
+        spots_data = validated_data.pop('spots')
+        spot_count = len(spots_data)
+        if spot_count < 2:
+            raise serializers.ValidationError({"spots": "This fields must have more than 2."})
+        lat, lon = 0, 0
+        plan = Plan.objects.create(user=user, **validated_data)
+        for spot_data in spots_data:
+            Spot.objects.create(plan=plan, **spot_data)
+            # 経度緯度を全て足し合わせる
+            lat += spot_data['lat']
+            lon += spot_data['lon']
+        # spotの数で割った平均値でPlanのlocationを決める
+        loc = convert_geo_to_location(lat / spot_count, lon / spot_count)
+        plan.location = Location.objects.create(
+            p_name=loc.p_name, p_code=loc.p_code, m_name=loc.m_name, m_code=loc.m_code
+        )
+        plan.save()
+        return plan
--- src/plan/tests.py
+++ src/plan/tests.py
@@ -1,3 +1,53 @@
-from django.test import TestCase
+import os
+import base64
+from rest_framework.test import APITestCase
+from rest_framework_jwt.settings import api_settings
 
-# Create your tests here.
+from django.conf import settings
+
+
+from accounts.models import User
+
+
+class PlanTest(APITestCase):
+
+    def setUp(self):
+        self.user_data = {"username": "test_user", "password": "hogefugapiyo"}
+        self.user = User.objects.create(**self.user_data, is_active=True)
+
+    def test_post(self):
+        self.assertEqual(1, User.objects.count())
+        img_file = os.path.join(settings.MEDIA_ROOT, "icons", "user.png")
+        with open(img_file, 'rb') as fp:
+            b64image = base64.encodebytes(fp.read())
+        post_data_set = {
+            "name": "嵐山コース",
+            "price": 10000,
+            "duration": 360,
+            "note": "嵐山でぶらぶらしながら色んなお店を回るコースです",
+            "spots": [
+                {
+                    "name": "嵐山公園",
+                    "order": 0,
+                    "lat": 35.012072,
+                    "lon": 135.6791853,
+                    "note": "いい公園",
+                    "image": "".format(b64image.decode("utf-8"))
+                }, {
+                    "name": "嵐山公園",
+                    "order": 1,
+                    "lat": 35.012072,
+                    "lon": 135.6791853,
+                    "note": "いい公園",
+                    "image": "".format(b64image.decode("utf-8"))
+                }
+            ]
+        }
+
+        jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
+        jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
+        payload = jwt_payload_handler(self.user)
+        token = jwt_encode_handler(payload)
+        self.client.credentials(HTTP_AUTHORIZATION="JWT " + token)
+        post_resp = self.client.post("/plan/plans/", data=post_data_set, format='json')
+        self.assertEqual(201, post_resp.status_code)
