1f4d4d3e6c09c6f008058d954707ee4c10223f96
Pudding <s.kokuryo@gmail.com>
2018-10-20 18:00:57 +0900

    add models to admin

--- src/plan/admin.py
+++ src/plan/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Location, Spot, Plan, Report
+
+
+class LocationInline(admin.TabularInline):
+    model = Location
+
+
+@admin.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ('p_name', 'p_code', 'm_name', 'm_code')
+
+
+class SpotInline(admin.StackedInline):
+    model = Spot
+    extra = 1
+
+
+@admin.register(Spot)
+class SpotAdmin(admin.ModelAdmin):
+    list_display = ('name', 'get_plan', 'created_at')
+
+    def get_plan(self):
+        return self.obj.plan.name
+
+
+@admin.register(Plan)
+class PlanAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'duration', 'created_at')
+
+    inlines = [SpotInline]
+
+
+@admin.register(Report)
+class ReportAdmin(admin.ModelAdmin):
+    list_display = ('get_plan', 'get_username', 'created_at')
+
+    def get_plan(self):
+        return self.obj.plan.name
+
+    def get_username(self):
+        return self.obj.user.username
