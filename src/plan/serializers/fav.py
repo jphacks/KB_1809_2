9febf170c0a16fc53892b32601332cfe8b2ec098
Pudding <s.kokuryo@gmail.com>
2018-10-20 23:21:34 +0900

    refs #41 Implement FavListSerializer

--- src/plan/serializers/fav.py
+++ src/plan/serializers/fav.py
@@ -6,6 +6,35 @@ from accounts.models import User
 from accounts.serializers import SimpleUserSerializer
 
 
+class FavListSerializer(serializers.ListSerializer):
+    """
+    複数のFavを処理するSerializer
+    """
+
+    def create(self, validated_data):
+        favs = [Fav(**item) for item in validated_data]
+        return Fav.objects.bulk_create(favs)
+
+    def update(self, instance, validated_data):
+        fav_mapping = {fav.id: fav for fav in instance}
+        data_mapping = {item['id']: item for item in validated_data}
+
+        ret = []
+        for fav_id, spot_data in data_mapping.items():
+            fav = fav_mapping.get(fav_id)
+            if fav:
+                # 既に存在するIDなので情報をアップデート
+                ret.append(self.child.update(fav, spot_data))
+            else:
+                # 存在しないので作成
+                ret.append(self.child.create(spot_data))
+        for fav_id, fav in fav_mapping.items():
+            if fav_id not in data_mapping:
+                # DBに存在するがPOSTされてきたデータに存在しないデータを削除
+                fav.delete()
+        return ret
+
+
 class FavSerializer(serializers.ModelSerializer):
     """
     単一のFavを処理するSerializer
@@ -17,6 +46,7 @@ class FavSerializer(serializers.ModelSerializer):
     class Meta:
         model = Fav
         fields = ("pk", "user", "plan")
+        list_serializer_class = FavListSerializer
 
     def to_internal_value(self, data):
         user_id = data.get('user_id')
@@ -33,6 +63,4 @@ class FavSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = User.objects.get(pk=validated_data.get('user_id'))
         plan = Plan.objects.get(pk=validated_data.get('plan_id'))
-
         return Fav.objects.create(user=user, plan=plan)
-
