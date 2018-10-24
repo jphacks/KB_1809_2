29930aa72fe4dd8a6f6abff600d7b7b79dc6949f
Pudding <s.kokuryo@gmail.com>
2018-10-25 00:27:49 +0900

    Add tests

--- src/accounts/tests.py
+++ src/accounts/tests.py
@@ -1,3 +1,71 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework_jwt.settings import api_settings
 
-# Create your tests here.
+from .models import User
+
+
+class UserTest(APITestCase):
+
+    def setUp(self):
+        super(UserTest, self).setUp()
+        self.user_data = {"username": "test_user", "password": "hogefugapiyo"}
+
+    def tearDown(self):
+        super(UserTest, self).tearDown()
+        User.objects.all().delete()
+
+    def _set_credentials(self, user):
+        jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
+        jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
+        payload = jwt_payload_handler(user)
+        token = jwt_encode_handler(payload)
+        self.client.credentials(HTTP_AUTHORIZATION="JWT " + token)
+
+    def test_create_user(self):
+        """POST /users/create/: Userの作成テスト"""
+        res = self.client.post('/users/create/', data=self.user_data, format='json')
+        self.assertEqual(201, res.status_code)
+        user = User.objects.all()
+        self.assertEqual(1, len(user))
+        self.assertEqual(self.user_data['username'], user[0].username)
+
+    def test_update_user(self):
+        """PATCH /me/: 自身の情報の更新テスト"""
+        user = User.objects.create_user(**self.user_data, is_active=True)
+        self._set_credentials(user)
+        res = self.client.patch('/me/', data={'email': 'hoge@fuga.com'}, format='json')
+        self.assertEqual(200, res.status_code)
+
+    def test_get_me(self):
+        """GET /me/: 自身の情報の取得テスト"""
+        user = User.objects.create_user(**self.user_data, is_active=True)
+        self._set_credentials(user)
+        res = self.client.get('/me/')
+        self.assertEqual(200, res.status_code)
+        self.assertEqual(user.username, res.data['username'])
+
+    def test_get_user(self):
+        """GET /users/<id>/: 特定のユーザ情報の取得テスト"""
+        user = User.objects.create_user(**self.user_data, is_active=True)
+        new_user_data = {'username': 'new_user', 'password': 'new_password'}
+        new_user = User.objects.create_user(**new_user_data, is_active=True)
+        self._set_credentials(user)
+        res = self.client.get('/users/{}/'.format(new_user.pk))
+        self.assertEqual(200, res.status_code)
+        self.assertEqual(new_user.username, res.data['username'])
+
+    def test_get_user_list(self):
+        """GET /users/: ユーザ全体の情報取得テスト"""
+        user = User.objects.create_user(**self.user_data, is_active=True)
+        self._set_credentials(user)
+        res = self.client.get('/users/')
+        self.assertEqual(403, res.status_code)
+
+    # Delete user fail. issue: https://github.com/sunscrapers/djoser/issues/257
+    # def test_delete_user(self):
+    #     """DELETE /users/me/: 自身のアカウントの削除テスト"""
+    #     user = User.objects.create_user(**self.user_data, is_active=True)
+    #     self._set_credentials(user)
+    #     res = self.client.delete('/users/me/', data={'current_password': self.user_data['password']})
+    #     self.assertEqual(204, res.status_code)
+    #     self.assertFalse(User.objects.filter(username=user.username).exists())
--- src/plan/tests.py
+++ src/plan/tests.py
@@ -7,14 +7,42 @@ from django.conf import settings
 
 
 from accounts.models import User
-from plan.models import Spot
+from .models import Plan
+
+img_file = os.path.join(settings.MEDIA_ROOT, "icons", "user.png")
+with open(img_file, 'rb') as fp:
+    b64image = base64.encodebytes(fp.read())
+plan_data = {
+    "name": "嵐山コース",
+    "price": 10000,
+    "duration": 360,
+    "note": "嵐山でぶらぶらしながら色んなお店を回るコースです",
+    "spots": [
+        {
+            "name": "嵐山公園",
+            "lat": 35.012072,
+            "lon": 135.6791853,
+            "note": "いい公園",
+            "image": b64image
+        }, {
+            "name": "嵐山公園",
+            "lat": 35.012072,
+            "lon": 135.6791853,
+            "note": "いい公園",
+            "image": b64image
+        }
+    ]
+}
 
 
-class PlanTest(APITestCase):
+class PlanListTest(APITestCase):
 
     def setUp(self):
         self.user_data = {"username": "test_user", "password": "hogefugapiyo"}
-        self.user = User.objects.create(**self.user_data, is_active=True)
+        self.user = User.objects.create_user(**self.user_data, is_active=True)
+        self.post_data_set = plan_data
+
+    def _set_credentials(self):
         jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
         jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
         payload = jwt_payload_handler(self.user)
@@ -22,32 +50,58 @@ class PlanTest(APITestCase):
         self.client.credentials(HTTP_AUTHORIZATION="JWT " + token)
 
     def test_post(self):
-        self.assertEqual(1, User.objects.count())
-        img_file = os.path.join(settings.MEDIA_ROOT, "icons", "user.png")
-        with open(img_file, 'rb') as fp:
-            b64image = base64.encodebytes(fp.read())
-        post_data_set = {
-            "name": "嵐山コース",
-            "price": 10000,
-            "duration": 360,
-            "note": "嵐山でぶらぶらしながら色んなお店を回るコースです",
-            "spots": [
-                {
-                    "name": "嵐山公園",
-                    "lat": 35.012072,
-                    "lon": 135.6791853,
-                    "note": "いい公園",
-                    "image": b64image
-                }, {
-                    "name": "嵐山公園",
-                    "lat": 35.012072,
-                    "lon": 135.6791853,
-                    "note": "いい公園",
-                    "image": b64image
-                }
-            ]
-        }
+        """POST /plan/plans/: プラン作成テスト"""
+        self._set_credentials()
+        res = self.client.post("/plan/plans/", data=self.post_data_set, format='json')
+        self.assertEqual(201, res.status_code)
 
-        self.assertEqual(0, Spot.objects.count())
-        post_resp = self.client.post("/plan/plans/", data=post_data_set, format='json')
-        self.assertEqual(201, post_resp.status_code)
+    def test_get(self):
+        """GET /plan/plans/: プラン取得テスト"""
+        self._set_credentials()
+        self.client.post("/plan/plans/", data=self.post_data_set, format='json')
+        new_data = self.post_data_set
+        new_data['name'] = "新しいコース"
+        self.client.post("/plan/plans/", data=new_data, format='json')
+        res = self.client.get('/plan/plans/')
+        self.assertEqual(res.data[0]['name'], new_data['name'])
+
+
+class PlanTest(APITestCase):
+
+    def setUp(self):
+        self.user_data = {"username": "test_user", "password": "hogefugapiyo"}
+        self.post_data_set = plan_data
+
+    def tearDown(self):
+        super(PlanTest, self).tearDown()
+        User.objects.all().delete()
+        Plan.objects.all().delete()
+
+    def _set_credentials(self, user):
+        jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
+        jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
+        payload = jwt_payload_handler(user)
+        token = jwt_encode_handler(payload)
+        self.client.credentials(HTTP_AUTHORIZATION="JWT " + token)
+
+    def test_get(self):
+        """GET /plan/plans/<id>/: Planの詳細を取得するテスト"""
+        user = User.objects.create_user(**self.user_data, is_active=True)
+        self._set_credentials(user)
+        self.client.post('/plan/plans/', data=self.post_data_set, format='json')
+        plan = Plan.objects.all()[0]
+        res = self.client.get('/plan/plans/{}/'.format(plan.pk))
+        self.assertEqual(200, res.status_code)
+        self.assertEqual(plan.name, res.data['name'])
+        self.assertEqual(2, len(res.data['spots']))
+
+    # TODO: Implement PlanSerializer.update()
+    # def test_patch(self):
+    #     """PATCH /plan/plans/<id>: 特定のPlanの更新テスト"""
+    #     user = User.objects.create_user(**self.user_data, is_active=True)
+    #     self._set_credentials(user)
+    #     res = self.client.post('/plan/plans/', data=self.post_data_set, format='json')
+    #     updated_data = self.post_data_set
+    #     updated_data['name'] = 'hoge'
+    #     res = self.client.patch('/plan/plans/{}/'.format(res.data['pk']), data=updated_data, format='json')
+    #     self.assertEqual(200, res.status_code)
