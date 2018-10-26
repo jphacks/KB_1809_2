3f1f3a942bbfb06552a20e86fbe83c49cefdc0a9
Pudding <s.kokuryo@gmail.com>
2018-10-26 14:23:49 +0900

    fix url base name

--- src/plan/urls.py
+++ src/plan/urls.py
@@ -3,12 +3,12 @@ from . import views
 
 
 router = DefaultRouter()
-router.register(r'locations', views.LocationViewSets, base_name='locations-list')
-router.register(r'spots', views.SpotViewSets, base_name='spots-list')
-router.register(r'reports', views.ReportViewSets, base_name='reports-list')
-router.register(r'comments', views.CommentViewSets, base_name='comments-list')
-router.register(r'favs', views.FavViewSets, base_name='favs-list')
-router.register(r'plans', views.PlanViewSets, basename='plans')
+router.register(r'locations', views.LocationViewSets, base_name='locations')
+router.register(r'spots', views.SpotViewSets, base_name='spots')
+router.register(r'reports', views.ReportViewSets, base_name='reports')
+router.register(r'comments', views.CommentViewSets, base_name='comments')
+router.register(r'favs', views.FavViewSets, base_name='favs')
+router.register(r'plans', views.PlanViewSets, base_name='plans')
 
 app_name = 'plan'
 urlpatterns = router.urls
