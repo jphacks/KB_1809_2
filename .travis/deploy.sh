e17b1b52008fbf64f849935d3c1f162ce789dcdf
Pudding <s.kokuryo@gmail.com>
2018-10-27 00:24:23 +0900

    exec `docker push` only on branch to deploy and not PR

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -3,7 +3,7 @@
 # normal deploy
 START_SERVER="make prod-start"
 MIGRATE="make prod-manage ARGS=migrate"
-COMMAND="cd /opt/KB_1809_2 && sudo git pull && make pull && $START_SERVER && $MIGRATE"
+COMMAND="cd /opt/KB_1809_2 && sudo git checkout $DEPLOY_BRANCH && sudo git pull && make pull && $START_SERVER && $MIGRATE"
 
 # exec
 ssh -o StrictHostKeyChecking=no -i /tmp/aquatan_studio deploy@aqua.aquatan.studio ${COMMAND}
