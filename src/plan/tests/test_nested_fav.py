5326a84ac1d555550dda7a0764db0df46734bead
Pudding <s.kokuryo@gmail.com>
2018-10-26 17:20:38 +0900

    optimize tests

--- src/plan/tests/test_nested_fav.py
+++ src/plan/tests/test_nested_fav.py
@@ -34,18 +34,14 @@ plan_data = {
     ]
 }
 
-post_data = {
-    "plan_id": 1
-}
-
 
 class FavTest(APITestCase):
 
     def setUp(self):
         self.user_data = {"username": "test_user", "password": "hogefugapiyo"}
         self.user = User.objects.create_user(**self.user_data, is_active=True)
-        self.post_data_set = post_data
         self._set_credentials()
+        self.plan_res = self.client.post("/plan/plans/", data=plan_data, format='json')
 
     def tearDown(self):
         super(FavTest, self).tearDown()
@@ -63,18 +59,16 @@ class FavTest(APITestCase):
 
     def test_post(self):
         """POST /plan/plans/<id>/favs/: いいね作成テスト"""
-        plan_res = self.client.post("/plan/plans/", data=plan_data, format='json')
         for path in ['/plan/plans/{}/favs/me/', '/plan/plans/{}/favs/']:
-            res = self.client.post(path.format(plan_res.data['pk']), data={}, format="json")
+            res = self.client.post(path.format(self.plan_res.data['pk']), data={}, format="json")
             self.assertEqual(201, res.status_code)
             Fav.objects.get(pk=res.data['pk']).delete()
 
     def test_get_list(self):
         """GET /plan/plans/<id>/favs/: いいね一覧取得テスト"""
-        res = self.client.post("/plan/plans/", data=plan_data, format='json')
-        plan_id = res.data['pk']
-        fav = Fav.objects.create(user=self.user, plan_id=res.data['pk'])
-        res = self.client.get("/plan/plans/{}/favs/".format(res.data['pk']), data={}, format="json")
+        plan_id = self.plan_res.data['pk']
+        fav = Fav.objects.create(user=self.user, plan_id=plan_id)
+        res = self.client.get("/plan/plans/{}/favs/".format(plan_id), data={}, format="json")
         self.assertEqual(200, res.status_code)
         actual_favs = Fav.objects.filter(plan_id=plan_id).count()
         # 全てそのplanのfavであることを確認
@@ -86,31 +80,27 @@ class FavTest(APITestCase):
 
     def test_destroy(self):
         """DELETE /plan/plans/<id>/favs/: いいね削除テスト"""
-        plan_res = self.client.post("/plan/plans/", data=plan_data, format='json')
-        fav = Fav.objects.create(user=self.user, plan_id=plan_res.data['pk'])
-        res = self.client.delete('/plan/plans/{}/favs/me/'.format(plan_res.data['pk']))
+        fav = Fav.objects.create(user=self.user, plan_id=self.plan_res.data['pk'])
+        res = self.client.delete('/plan/plans/{}/favs/me/'.format(self.plan_res.data['pk']))
         self.assertEqual(204, res.status_code)
         with self.assertRaises(Fav.DoesNotExist):
             Fav.objects.get(pk=fav.pk)
 
     def test_destroy_fail(self):
         """DELETE /plan/plans/<id>/favs/: 存在しないいいねの削除テスト"""
-        plan_res = self.client.post("/plan/plans/", data=plan_data, format='json')
-        res = self.client.delete('/plan/plans/{}/favs/me/'.format(plan_res.data['pk']))
+        res = self.client.delete('/plan/plans/{}/favs/me/'.format(self.plan_res.data['pk']))
         self.assertEqual(404, res.status_code)
 
     def test_get_detail(self):
         """GET /plan/plans/<id>/favs/<id>/: いいねの詳細取得テスト"""
-        plan_res = self.client.post("/plan/plans/", data=plan_data, format='json')
-        fav = Fav.objects.create(user=self.user, plan_id=plan_res.data['pk'])
-        res = self.client.get("/plan/plans/{}/favs/{}/".format(plan_res.data['pk'], fav.pk), data={}, format="json")
+        fav = Fav.objects.create(user=self.user, plan_id=self.plan_res.data['pk'])
+        res = self.client.get("/plan/plans/{}/favs/{}/".format(self.plan_res.data['pk'], fav.pk), data={}, format="json")
         self.assertEqual(200, res.status_code)
         self.assertEqual(self.user.username, res.data['user']['username'])
 
     def test_get_detail_404(self):
         """GET /plan/plans/<id>/favs/<id>/: いいねの詳細取得404テスト"""
-        plan_res = self.client.post("/plan/plans/", data=plan_data, format='json')
-        res = self.client.get("/plan/plans/{}/favs/{}/".format(plan_res.data['pk'], 9999), data={}, format="json")
+        res = self.client.get("/plan/plans/{}/favs/{}/".format(self.plan_res.data['pk'], 9999), data={}, format="json")
         self.assertEqual(404, res.status_code)
         new_plan_res = self.client.post("/plan/plans/", data=plan_data, format='json')
         fav = Fav.objects.create(user=self.user, plan_id=new_plan_res.data['pk'])
