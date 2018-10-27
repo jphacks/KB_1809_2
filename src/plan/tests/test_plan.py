bb69f55b8ec0edac19cf117e39142b500c5f4d7c
Pudding <s.kokuryo@gmail.com>
2018-10-27 15:36:20 +0900

    update test for Plan to use BaseTestCase

--- src/plan/tests/test_plan.py
+++ src/plan/tests/test_plan.py
@@ -1,37 +1,14 @@
-import copy
-from rest_framework.test import APITestCase
-from rest_framework_jwt.settings import api_settings
-
-from accounts.models import User
 from plan.models import Plan
-from .data import plan_data
+from .base import BaseTestCase
 
 
-class PlanTest(APITestCase):
-
-    def setUp(self):
-        self.user_data = {"username": "test_user", "password": "hogefugapiyo"}
-        self.post_data_set = copy.deepcopy(plan_data)
-
-    def tearDown(self):
-        super(PlanTest, self).tearDown()
-        User.objects.all().delete()
-        Plan.objects.all().delete()
-
-    def _set_credentials(self, user):
-        jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
-        jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
-        payload = jwt_payload_handler(user)
-        token = jwt_encode_handler(payload)
-        self.client.credentials(HTTP_AUTHORIZATION="JWT " + token)
+class PlanTest(BaseTestCase):
 
     def test_get(self):
         """GET /plan/plans/<id>/: Planの詳細を取得するテスト"""
-        user = User.objects.create_user(**self.user_data, is_active=True)
-        self._set_credentials(user)
-        self.client.post('/plan/plans/', data=self.post_data_set, format='json')
+        self.client.post(self.plan_path, data=self.plan_data, format='json')
         plan = Plan.objects.all()[0]
-        res = self.client.get('/plan/plans/{}/'.format(plan.pk))
+        res = self.client.get(self.plan_detail_path.format(plan.pk))
         self.assertEqual(200, res.status_code)
         self.assertEqual(plan.name, res.data['name'])
         self.assertEqual(2, len(res.data['spots']))
