80fb92e24e7a6d63cf23a6bb9f03284a7cb363c2
taxio <taxio.2029@gmail.com>
2018-10-27 19:41:06 +0900

    add delete option and refactor

--- src/plan/management/commands/clearmedia.py
+++ src/plan/management/commands/clearmedia.py
@@ -8,26 +8,29 @@ import pathlib
 
 
 class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--delete',
+            action='store_true',
+            dest='delete',
+            default=False,
+            help='Delete all no use image files in medir directory.'
+        )
+
     def handle(self, *args, **options):
         used_imgs = list()
-        users = User.objects.all()
-        for u in users:
-            used_imgs.append(u.icon.path)
-        spots = Spot.objects.all()
-        for s in spots:
-            used_imgs.append(s.image.path)
-        reports = Report.objects.all()
-        for r in reports:
-            used_imgs.append(r.image.path)
+        used_imgs.extend([u.icon.path for u in User.objects.all()])
+        used_imgs.extend([s.image.path for s in Spot.objects.all()])
+        used_imgs.extend([r.image.path for r in Report.objects.all()])
 
         p = pathlib.Path(settings.MEDIA_ROOT)
         for f in p.glob("**/*"):
-            if f.is_dir():
-                continue
-
-            if f.name == "user.png":
+            if f.is_dir() or f.name == "user.png":
                 continue
 
             if f.as_posix() not in used_imgs:
-                print("remove", f.as_posix())
-                f.unlink()
+                if options['delete']:
+                    print("remove", f.as_posix())
+                    f.unlink()
+                else:
+                    print(f.as_posix())
