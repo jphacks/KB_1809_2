7c1197415e98e49022e541120e9567a26d265741
taxio <taxio.2029@gmail.com>
2018-11-05 15:05:41 +0900

    improve filter

--- src/plan/filters.py
+++ src/plan/filters.py
@@ -18,17 +18,29 @@ class LocationFilter(filters.FilterSet):
 
 class PlanLocationFilter(filters.FilterSet):
     """
-    Planを都道府県名や市区町村名でフィルタする (or)
+    Planを都道府県名や市区町村名，スポット名でフィルタする (or)
     """
-    location = filters.CharFilter(method='filter_p_and_m_name')
+    location = filters.CharFilter(method='filter_words')
 
     class Meta:
         models = models.Plan
         fields = ('location',)
 
-    def filter_p_and_m_name(self, queryset, name, value):
-        return queryset.filter(
-            Q(location__p_name__contains=value) | Q(location__m_name__contains=value)
-        )
+    def filter_words(self, queryset, name, value):
+        if self.request.version == "v1":
+            return queryset.filter(
+                Q(location__p_name__contains=value) | Q(location__m_name__contains=value)
+            )
+
+        querys = list()
+        for word in value.split(" "):
+            querys.append(Q(location__p_name__contains=word))
+            querys.append(Q(location__m_name__contains=word))
+            querys.append(Q(name__contains=word))
+            querys.append(Q(spots__name__contains=word))
+        query = querys.pop()
+        for q in querys:
+            query |= q
+        return queryset.filter(query).distinct()
 
 
